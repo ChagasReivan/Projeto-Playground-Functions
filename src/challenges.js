6c4a8dbf8b8c88c242735c8f3469528675d80aff
Reivan Chagas <chagasreivan@gmail.com>
2022-08-24 17:31:56 -0300

    Adiciona resolução dos desafios(1 ao 6)

--- src/challenges.js
+++ src/challenges.js
@@ -1,32 +1,57 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valor1,valor2) {
+  return valor1 && valor2;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return ((base * height)/2);
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string){
+  let separador = string.split(' ')
+  return separador;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+
+function concatName(lista) {
+  let ultimaP = lista[lista.length -1];
+  let primeiraP = lista[0];
+  let posicaoCerta = ultimaP +', '+primeiraP;
+  return posicaoCerta;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+ let points = (wins*3) + (ties*1);
+ return points;
+
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(numeros) {
+  let maiorNumero = numeros[0];
+  let resultado = 0;
+  for(let index =1; index < numeros.length; index +=1){
+    if(numeros[index] > maiorNumero){
+      maiorNumero = numeros[index];
+    }
+  }
+  for ( let index =0; index < numeros.length; index +=1){
+    if(numeros[index] === maiorNumero){
+      resultado +=1;
+     }
+  }
+  return resultado;
+  }
+  
+  
+    
+  
+  
+
 
 // Desafio 7
 function catAndMouse() {
